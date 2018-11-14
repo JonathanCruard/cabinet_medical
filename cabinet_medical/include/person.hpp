b54c0d7cdec94976139cecb46d5f938028dab2dc
JonathanCruard <jonathan.cruard@gmail.com>
2018-11-14 11:19:06 +0100

    hpp completed

--- cabinet_medical/include/person.hpp
+++ cabinet_medical/include/person.hpp
@@ -8,20 +8,27 @@ class person
 {
 protected:
     //class variable
-    string first_name;
-    string last_name;
+    string _first_name;
+    string _last_name;
+    unsigned int _id;
 
 //class method
     //setters
     void set_f_name(string name);
     void set_l_name(string name);
+    void set_id();
 
     //getters
     string get_f_name();
     string get_l_name();
-
+    int get_id();
     //affichage
     void display();
+
+public:
+    //constructors
+    person();
+    person(string first_name, string last_name)
 
 };
 
--- cabinet_medical/include/prescription.hpp
+++ cabinet_medical/include/prescription.hpp
@@ -5,20 +5,26 @@ class prescription
 {
 private:
 //class variables
-    int prescriptor;
-    date prescription_date;
-    drugs_struct drugs;
+    int _prescriptor;
+    date _prescription_date;
+    list<drug_struct> _drugs;
 
 //class methods
     //setters
     void set_prescripteur(int doctor);
-    void set_prescription_date(date date);
-    void set_drugs(drugs_struct drugs);
+    void set_date(date date);
+    void append_drug(drug_struct drug);
     //getters
     int get_prescriptor();
     date get_date();
     drugs get_drugs();
 
+public:
+    //constructors
+    prescription();
+    prescription(int prescriptor, date date);
+    prescription(int prescriptor, date date, list<drug_struct>);
+    //TODO à revoir
 };
 
 
