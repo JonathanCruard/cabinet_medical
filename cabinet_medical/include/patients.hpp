5d5e9c51d9dd1ae8e0573bf42506954bfac654a0
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-14 10:18:13 +0100

    progress

--- cabinet_medical/include/patients.hpp
+++ cabinet_medical/include/patients.hpp
@@ -2,11 +2,28 @@
 #define DEF_PATIENT
 
 #include "person.hpp"
-
+#include "global.hpp"
 
 class patient : public person
 {
+public:
+//Class methods
+    //setters
+    void set_blood_group(string blood_group);
+    void set_security_number(int security_number);
+    void set_telephon(int _telephon_number);
+    //getters
+    string get_blood_group();
+    int get_security_number();
+    int get_telephon();
 
+//Methods
+
+private:
+    //Variable
+        string _blood_group;
+        int _security_number;
+        int _telephon_number;
 };
 
 
--- cabinet_medical/src/patients.cpp
+++ cabinet_medical/src/patients.cpp
@@ -1,2 +1,8 @@
 #include "patients.hpp"
 #include "person.hpp"
+
+class patients : public person
+{
+
+
+}
