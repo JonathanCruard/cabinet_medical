4117e64b4442cef6ed69c4163ab32ff4a74d210a
JonathanCruard <jonathan.cruard@gmail.com>
2018-11-14 10:36:58 +0100

    prescription ok normalement

--- cabinet_medical/include/prescription.hpp
+++ cabinet_medical/include/prescription.hpp
@@ -5,13 +5,19 @@ class prescription
 {
 private:
 //class variables
-    int prescripteur;
+    int prescriptor;
     date prescription_date;
     drugs_struct drugs;
+
 //class methods
     //setters
-
+    void set_prescripteur(int doctor);
+    void set_prescription_date(date date);
+    void set_drugs(drugs_struct drugs);
     //getters
+    int get_prescriptor();
+    date get_date();
+    drugs get_drugs();
 
 };
 
