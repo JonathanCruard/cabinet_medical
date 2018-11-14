abc3ae78718e0ebd9cae13b8340b8f954581bb9f
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-14 10:32:55 +0100

    include all hpp

--- cabinet_medical/src/main.cpp
+++ cabinet_medical/src/main.cpp
@@ -1,6 +1,14 @@
 #include <iostream>
 using namespace std;
 
+#include "person.hpp"
+#include "patient.hpp"
+#include "doctor.hpp"
+#include "meeting.hpp"
+#include "prescription.hpp"
+#include "global.hpp"
+#include "date.hpp"
+
 int main() 
 {
     cout << "Hello, World!";
