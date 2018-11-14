ceb854007055b2e1ddf058e1abc5d0512d694215
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-14 18:18:55 +0100

    setter, getters and constructors

--- cabinet_medical/src/date.cpp
+++ cabinet_medical/src/date.cpp
@@ -1,1 +1,56 @@
 #include "date.hpp"
+
+//==============================================================================
+// Constructors
+//==============================================================================
+date::date()
+{
+    set_year(0);
+    set_month(0);
+    set_day(0);
+}
+date::date(unsigned int year, unsigned int month, unsigned int day)
+{
+    set_year(year);
+    set_month(month);
+    set_day(day);
+}
+
+//==============================================================================
+// Setters
+//==============================================================================
+
+date::set_year(unsigned int year)
+{
+    _year = year;
+}
+
+date::set_month(unsigned int month)
+{
+    _month = month;
+}
+
+date::set_day(unsigned int day)
+{
+    _day = day;
+}
+
+
+//==============================================================================
+// Getters
+//==============================================================================
+
+date::get_year()
+{
+    return _year;
+}
+
+date::get_month()
+{
+    return _month;
+}
+
+date::get_day()
+{
+    return _day;
+}
