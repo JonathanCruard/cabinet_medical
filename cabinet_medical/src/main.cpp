ba95a4bf7c377d93383331e92737b05d35495e81
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-21 12:04:39 +0100

    add a switch

--- cabinet_medical/src/main.cpp
+++ cabinet_medical/src/main.cpp
@@ -11,6 +11,19 @@ using namespace std;
 
 int main()
 {
+    int choice = 1;
+    while (choice != 0) {
+        std::cout << "*************Welcome to Arkham Asylum:*************" << '\n' << "To control this program please selct one of those possibilities:"<< '\n';
+        std::cout << "1: For add new patients" << '\n' << "1: For add new doctors"<< '\n';
+        std::cin >> choice;
+        switch (choice) {
+            case 1:
+                break;
+            case 2:
+            default:
+                break;
+        }
+    }
     patient pati("Ab",12, 15);
     string blood_group = pati.get_blood_group();
     std::cout << blood_group << '\n';
