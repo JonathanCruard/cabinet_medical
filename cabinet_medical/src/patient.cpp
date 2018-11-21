e4e5b0724618bfa3e7f0efee72b6951f486a16bd
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-21 11:42:34 +0100

    verbose

--- cabinet_medical/src/patient.cpp
+++ cabinet_medical/src/patient.cpp
@@ -6,9 +6,10 @@
 //==========================================================================
 patient::patient() : person()
 {
-    set_blood_group("");
-    set_security_number(0);
-    set_phone(0);
+    set_blood_group();
+    set_security_number();
+    set_phone();
+
 }
 patient::patient(string blood_group, int security_number, int phone_number) : person()
 {
@@ -34,14 +35,17 @@ void patient::set_phone(int phone_number)
 
 void patient::set_blood_group()
 {
+    std::cout << "Enter your blood_group" << '\n';
     std::cin >> _blood_group;
 }
 void patient::set_security_number()
 {
+    std::cout << "Enter your security_number" << '\n';
     std::cin >> _security_number;
 }
 void patient::set_phone()
 {
+    std::cout << "Enter your phone" << '\n';
     std::cin >> _phone_number;
 }
 //==========================================================================
