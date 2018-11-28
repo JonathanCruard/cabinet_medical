fbdbf34464b432b5849c6d7815a27b035057b035
tanguy.lallemand <tanguy.lallemand@gmail.com>
2018-11-28 10:31:55 +0100

    json handle seems to be ok

--- cabinet_medical/include/json_gestion.hpp
+++ cabinet_medical/include/json_gestion.hpp
@@ -1,1 +1,17 @@
+#ifndef DEF_PATIENT
+#define DEF_PATIENT
+
 #include <json/json.h>
+
+
+#include "global.hpp"
+
+
+class json_gestion
+{
+public:
+    void json_read();
+    void json_write();
+};
+
+#endif
--- cabinet_medical/src/json_gestion.cpp
+++ cabinet_medical/src/json_gestion.cpp
@@ -1,10 +1,45 @@
 #include "json_gestion.hpp"
 #include <json/json.h>
+#include <fstream>
 
-json_gestion::read_file()
+void json_gestion::json_read()
 {
+    Json::Value root;   // will contains the root value after parsing.
+    Json::Reader reader;
+    ifstream ifile("./data/test.json");
+    bool parsingSuccessful = reader.parse( ifile, root );
+
+    if (parsingSuccessful) {
+
+        const Json::Value patient = root.get("patient", "");
+        if (patient.isObject()) {
+
+            Json::Value fname = patient.get("fname", "");
+            Json::Value lname = patient.get("lname", "");
+            Json::Value blood_group = patient.get("blood_group", "");
+            Json::Value phone_number = patient.get("phone_number", "");
+            Json::Value security_number = patient.get("security_number", "");
+            cout << "fname : " << fname.asString() << endl;
+            cout << "lname : " << lname.asString() << endl;
+            cout << "blood_group : " << blood_group.asString() << endl;
+            cout << "phone_number : " << phone_number.asInt() << endl;
+            cout << "security_number : " << security_number.asInt() << endl;
+        } else {
+            cout << "no object type" << endl;
+        }
+    } else
+        {cout << "json is not correct format" << endl;}
+}
+void json_gestion::json_write()
+{
+    // will contains the root value after parsing.
     Json::Value root;
-    std::ifstream file("test.json");
-    file >> root;
+    Json::StyledWriter writer;
+    // Make a new JSON document for the configuration. Preserve original comments.
+    std::string outputConfig = writer.write( root );
+    // You can also use streams.  This will put the contents of any JSON
+    // stream at a particular sub-value, if you'd like.
+    std::cin >> root["subtree"];
+    // And you can write to a stream, using the StyledWriter automatically.
     std::cout << root;
 }
